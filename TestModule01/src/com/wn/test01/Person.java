181ad090566845e4598fe390d53bdb98854fc161
wangnan <wn@123.com>
2022-04-06 06:07:26 +0800

    在Person中增加了属性

---
+++ TestModule01/src/com/wn/test01/Person.java
@@ -0,0 +1,11 @@
+package com.wn.test01;
+
+/**
+ * @Auther: WangNan
+ * @Date: 2022/4/6 - 04 - 06 - 6:05
+ * @Description: com.wn.test01
+ * @version: 1.0
+ */
+public class Person {
+    private int age;
+}
